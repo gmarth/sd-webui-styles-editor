b4a496ae5eed1c2903735d2e0f3017178984a6e4
Alexander Thiess <thiess.alexander@googlemail.com>
2023-07-01 06:45:22 +0200

    Dropdown test

--- scripts/style_editor_tab.py
+++ scripts/style_editor_tab.py
@@ -7,7 +7,12 @@ from modules import script_callbacks
 
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
+
         with gr.Row():
+            styles = gr.Dropdown(
+                choices=["Choice 1", "Choice 2", "Choice 3", "Choice 4", "Choice 5"],
+                show_label=True
+            )
             angle = gr.Slider(
                 minimum=0.0,
                 maximum=360.0,
